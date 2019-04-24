380c9520e4c3e44c81acfe7cb9916cc55259480a
wangwei <895793323@qq.com>
2019-04-24 14:40:13 +0800

    ruisong-step4

--- src/app/config/reportDetails.ts
+++ src/app/config/reportDetails.ts
@@ -70,7 +70,7 @@ export const SCHEDULES = [
             {content: '领取证照', tip: '领取营业执照，刻制公章', date: '2019.2.21', executive: '易创注册部 陈瑜波', complete: 'ture'},
             {content: '银行开户', tip: '招商银行会展中心支行', date: '2019.2.22', executive: '易创注册部 陈瑜波', complete: 'ture'},
             {content: '税务落户', tip: '小规模纳税人', date: '2019.2.25', executive: '易创注册部 陈瑜波', complete: 'ture'},
-            {content: '增资扩股，完成投资人引入', tip: '', date: '2019.3.10', executive: '易创注册部 陈瑜波', complete: 'false'},
+            {content: '增资扩股，完成投资人引入', tip: '', date: '2019.3.10', executive: '易创注册部 陈瑜波', complete: 'ture'},
             {content: '设立员工持股平台', tip: '股权投资合伙有限企业', date: ' 2019.8.20', executive: '易创注册部 陈瑜波', complete: 'false'},
             {content: '其他安排事项', tip: '', date: ' ...', executive: '...', complete: 'false'},
         ]
