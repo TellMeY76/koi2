340deeb1ad6a3b17a7702a62c1377539f2890260
wangwei <895793323@qq.com>
2019-04-24 16:32:56 +0800

    update config

--- src/app/config/notice.ts
+++ src/app/config/notice.ts
@@ -7,7 +7,7 @@ export const NOTICE: Notice = {
         headImg: '../../assets/img/header.png'
     },
     finished: ['创新型初创企业备案', '科技型企业认定', '1季度个人所得税筹划...'],
-    Upcoming: ['创新券网站注册备案', '一般纳税人2季度税收筹划', '2季度企业所得税预估及筹划'],
+    Upcoming: ['进出口权证办理', '创新券网站注册备案', '一般纳税人2季度税收筹划', '2季度企业所得税预估及筹划'],
     deadline: 'Mar 25, 2019 00:00:00',
     tip: {}
 };
